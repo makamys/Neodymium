ddc6bb4731665a70cdbe17f52367c8accb254cd1
makamys <makamys@outlook.com>
2021-05-08 22:02:15 +0200

    Disable blending (fixes derpy fancy leaves)
    I really should force fast leaves on LOD models

--- src/main/java/makamys/lodmod/renderer/LODRenderer.java
+++ src/main/java/makamys/lodmod/renderer/LODRenderer.java
@@ -199,8 +199,7 @@ public class LODRenderer {
 	    GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
 	    GL11.glDisable(GL11.GL_TEXTURE_2D);
 
-	    GL11.glEnable(GL11.GL_BLEND);
-	    GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+	    GL11.glDisable(GL11.GL_BLEND);
 	    
 	    glUseProgram(shaderProgram);
 	    
