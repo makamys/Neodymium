b2baf9ac3a9c1671130082626c4bfdc8b93bee1c
makamys <makamys@outlook.com>
2022-06-09 13:54:20 +0200

    Disable unused configs

--- src/main/java/makamys/neodymium/Config.java
+++ src/main/java/makamys/neodymium/Config.java
@@ -13,17 +13,6 @@ import net.minecraftforge.common.config.Configuration;
 public class Config {
 
     public static boolean enabled;
-    public static int chunkLoadsPerTick;
-    public static List<Class<?>> blockClassBlacklist;
-    public static double fogStart;
-    public static double fogEnd;
-    public static double farPlaneDistanceMultiplier;
-    public static float maxSimpleMeshHeight;
-    public static boolean forceVanillaBiomeTemperature;
-    public static boolean hideUnderVanillaChunks;
-    public static boolean disableChunkMeshes;
-    public static boolean disableSimpleMeshes;
-    public static boolean saveMeshes;
     public static boolean optimizeChunkMeshes;
     public static int maxMeshesPerFrame;
     public static int sortFrequency;
@@ -31,6 +20,19 @@ public class Config {
     public static int VRAMSize;
     public static int debugPrefix;
     public static int debugInfoStartY;
+    public static boolean enableFog;
+    
+    // Unused LOD stuff
+    public static int chunkLoadsPerTick = 64;
+    public static List<Class<?>> blockClassBlacklist = Arrays.asList();
+    public static double fogStart = 0.25f;
+    public static double fogEnd = 1f;
+    public static double farPlaneDistanceMultiplier = 1;
+    public static boolean forceVanillaBiomeTemperature = false;
+    public static boolean hideUnderVanillaChunks = false;
+    public static boolean disableChunkMeshes = false;
+    public static boolean disableSimpleMeshes = true;
+    public static boolean saveMeshes = false;
     
     static File configFile;
     
@@ -39,6 +41,23 @@ public class Config {
         
         config.load();
         enabled = config.get("General", "enabled", true).getBoolean();
+        
+        optimizeChunkMeshes = config.getBoolean("optimizeChunkMeshes", "render", true, "");
+        maxMeshesPerFrame = config.getInt("maxMeshesPerFrame", "render", -1, -1, Integer.MAX_VALUE, "");
+        sortFrequency = config.getInt("sortFrequency", "render", 1, 1, Integer.MAX_VALUE, "");
+        gcRate = config.getInt("gcRate", "render", 1, 1, Integer.MAX_VALUE, "Maximum number of meshes to relocate each frame.");
+        VRAMSize = config.getInt("VRAMSize", "render", 1024, 1, Integer.MAX_VALUE, "VRAM buffer size (MB).");
+        enableFog = config.getBoolean("enableFog", "render", true, "");
+        debugPrefix = config.getInt("debugPrefix", "debug", Keyboard.KEY_F4, -1, Integer.MAX_VALUE, "This key has to be held down while pressing the debug keybinds. LWJGL keycode. Setting this to 0 will make the keybinds usable without holding anything else down. Setting this to -1 will disable debug keybinds entirely.");
+        debugInfoStartY = config.getInt("debugInfoStartY", "debug", 80, -1, Integer.MAX_VALUE, "The Y position of the first line of the debug info in the F3 overlay. Set this to -1 to disable showing that info.");
+        
+        if(config.hasChanged()) {
+            config.save();
+        }
+    }
+    
+    // Unused
+    public static void loadConfigLOD(Configuration config) {
         chunkLoadsPerTick = config.get("General", "chunkLoadsPerTick", 64).getInt();
         blockClassBlacklist = Arrays.stream(config.get("General", "blockClassBlacklist", "net.minecraft.block.BlockRotatedPillar;biomesoplenty.common.blocks.BlockBOPLog;gregapi.block.multitileentity.MultiTileEntityBlock").getString().split(";"))
                 .map(className -> {
@@ -53,29 +72,11 @@ public class Config {
         fogStart = config.get("Fog", "fogStart", "0.4").getDouble();
         fogEnd = config.get("Fog", "fogEnd", "0.8").getDouble();
         farPlaneDistanceMultiplier = config.get("Fog", "farPlaneDistanceMultiplier", "1.0").getDouble();
-        
-        maxSimpleMeshHeight = (float)config.get("Debug", "maxSimpleMeshHeight", 1000.0).getDouble();
-        
         forceVanillaBiomeTemperature = config.get("Simple mesh generation", "forceVanillaBiomeTemperature", true).getBoolean();
-        
         hideUnderVanillaChunks = config.getBoolean("hideUnderVanillaChunks", "render", true, "");
         disableChunkMeshes = config.getBoolean("disableChunkMeshes", "render", true, "");
         disableSimpleMeshes = config.getBoolean("disableSimpleMeshes", "render", false, "");
-        optimizeChunkMeshes = config.getBoolean("optimizeChunkMeshes", "render", true, "");
         saveMeshes = config.getBoolean("saveMeshes", "render", false, "");
-        maxMeshesPerFrame = config.getInt("maxMeshesPerFrame", "render", -1, -1, Integer.MAX_VALUE, "");
-        sortFrequency = config.getInt("sortFrequency", "render", 1, 1, Integer.MAX_VALUE, "");
-        gcRate = config.getInt("gcRate", "render", 1, 1, Integer.MAX_VALUE, "Maximum number of meshes to relocate each frame.");
-        VRAMSize = config.getInt("VRAMSize", "render", 1024, 1, Integer.MAX_VALUE, "VRAM buffer size (MB).");
-        enableFog = config.getBoolean("enableFog", "render", true, "");
-        debugPrefix = config.getInt("debugPrefix", "debug", Keyboard.KEY_F4, -1, Integer.MAX_VALUE, "This key has to be held down while pressing the debug keybinds. LWJGL keycode. Setting this to 0 will make the keybinds usable without holding anything else down. Setting this to -1 will disable debug keybinds entirely.");
-        debugInfoStartY = config.getInt("debugInfoStartY", "debug", 80, -1, Integer.MAX_VALUE, "The Y position of the first line of the debug info in the F3 overlay. Set this to -1 to disable showing that info.");
-        
-        if(config.hasChanged()) {
-            config.save();
-        }
     }
-
-    public static boolean enableFog;
     
 }
