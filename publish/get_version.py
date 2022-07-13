dc581acc5421f73d9436a4d86ef815d73e878b6d
makamys <makamys@outlook.com>
2022-07-13 15:29:22 +0200

    buildscript: Fix subprocess invocation

--- publish/get_version.py
+++ publish/get_version.py
@@ -9,6 +9,6 @@ if os.path.exists(versionPath):
     with open(versionPath, "r", encoding="utf8") as fp:
         ver = fp.read()
 else:
-    ver = subprocess.run("git describe --tags --dirty", capture_output=True, text=True).stdout or "UNKNOWN-" + subprocess.run("git describe --always --dirty", capture_output=True, text=True).stdout or "UNKNOWN"
+    ver = subprocess.run(["git", "describe", "--tags", "--dirty"], capture_output=True, text=True).stdout or "UNKNOWN-" + subprocess.run(["git", "describe", "--always", "--dirty"], capture_output=True, text=True).stdout or "UNKNOWN"
 
 print(ver.strip())
