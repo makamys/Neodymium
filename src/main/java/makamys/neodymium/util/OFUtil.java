3aa975f0a53a9d95fef1b4efffaf1483f9e7945a
makamys <makamys@outlook.com>
2023-12-02 15:09:58 +0100

    Don't apply OF compat if OF has failed to load
    Should fix reports like https://github.com/makamys/Neodymium/issues/13

--- src/main/java/makamys/neodymium/util/OFUtil.java
+++ src/main/java/makamys/neodymium/util/OFUtil.java
@@ -1,13 +1,19 @@
 package makamys.neodymium.util;
 
-import makamys.neodymium.MixinConfigPlugin;
+import static makamys.neodymium.Constants.LOGGER;
+
+import java.lang.reflect.Field;
+
 import makamys.neodymium.ducks.IMixinGameSettings_OptiFine;
 import net.minecraft.client.Minecraft;
 
 public class OFUtil {
-    private static boolean isOptiFinePresent = MixinConfigPlugin.class.getResource("/optifine/OptiFineTweaker.class") != null;
+    private static boolean isOptiFinePresent, hasCheckedIsOptiFinePresent;
     
     public static boolean isOptiFinePresent() {
+        if(!hasCheckedIsOptiFinePresent) {
+            checkIfOptiFineIsPresent();
+        }
         return isOptiFinePresent;
     }
     
@@ -15,6 +21,30 @@ public class OFUtil {
         return isOptiFinePresent && getIsFogOff();
     }
     
+    private static void checkIfOptiFineIsPresent() {
+        try {
+            Class<?> optiFineClassTransformerClass = Class.forName("optifine.OptiFineClassTransformer");
+            Field instanceField = optiFineClassTransformerClass.getField("instance");
+            Object optiFineClassTransformer = instanceField.get(null);
+            Field ofZipFileField = optiFineClassTransformerClass.getDeclaredField("ofZipFile");
+            ofZipFileField.setAccessible(true);
+            Object ofZipFile = ofZipFileField.get(optiFineClassTransformer);
+            
+            if(ofZipFile != null) {
+                LOGGER.debug("OptiFineClassTransformer#ofZipFile is initialized, assuming OptiFine is present");
+                isOptiFinePresent = true;
+            } else {
+                LOGGER.debug("OptiFineClassTransformer#ofZipFile is null, assuming OptiFine is not present");
+            }
+        } catch (ClassNotFoundException e) {
+            LOGGER.debug("Couldn't load OptiFineClassTransformer class, assuming OptiFine is not present");
+            // no OF
+        } catch(Exception e) {
+            LOGGER.error("Failed to check if OptiFine is loaded, assuming it isn't. Exception: " + e);
+        }
+        hasCheckedIsOptiFinePresent = true;
+    }
+
     private static boolean getIsFogOff() {
         return ((IMixinGameSettings_OptiFine)Minecraft.getMinecraft().gameSettings).getOfFogType() == 3;
     }
