makamys/Neodymium

commit ad42f10bad1aa7417ebeb9c3c1f31cd5b19b9220

stop renderlists logic from running at all

src/main/java/makamys/neodymium/renderer/NeoRenderer.java
@@ -104,7 +104,8 @@ public class NeoRenderer {
 
     Vector4f transformedOrigin = new Vector4f();
 
-    public void preRenderSortedRenderers(int renderPass, double alpha, WorldRenderer[] sortedWorldRenderers) {
+    public int preRenderSortedRenderers(int renderPass, double alpha, WorldRenderer[] sortedWorldRenderers) {
+        int rendered = 0;
         if (hasInited) {
             if (renderPass == 0) {
                 renderedMeshes = 0;
@@ -149,11 +150,12 @@ public class NeoRenderer {
             if (rendererActive && renderWorld) {
                 Minecraft.getMinecraft().entityRenderer.enableLightmap((double) alpha);
 
-                render(renderPass, alpha);
+                rendered += render(renderPass, alpha);
 
                 Minecraft.getMinecraft().entityRenderer.disableLightmap((double) alpha);
             }
         }
+        return rendered;
     }
 
     public void onRenderTickEnd() {
@@ -285,17 +287,16 @@ public class NeoRenderer {
     Matrix4f modelViewMatrixInv = new Matrix4f();
     Matrix4f projMatrix = new Matrix4f();
 
-    private void render(int pass, double alpha) {
-        int shader = getShaderProgram(pass);
-
-        if (shader == 0) return;
-
-        glBindVertexArray(VAO);
-
+    private int render(int pass, double alpha) {
         if (!Compat.isShaders()) {
+            int shader = getShaderProgram(pass);
+
+            if (shader == 0) return 0;
             glUseProgram(shader);
             updateUniforms(alpha, pass);
         }
+
+        glBindVertexArray(VAO);
 
         if (isWireframeEnabled()) {
             GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
@@ -308,9 +309,11 @@ public class NeoRenderer {
 
         int oldLimit = piFirst[pass].limit();
 
+        int rendered = 0;
         int order = pass == 0 ? 1 : -1;
         for (int regionI = order == 1 ? 0 : loadedRegionsList.size() - 1; regionI >= 0 && regionI < loadedRegionsList.size(); regionI += order) {
             NeoRegion.RenderData region = loadedRegionsList.get(regionI).getRenderData();
+            rendered += region.batchLimit[pass] - region.batchFirst[pass];
             Util.setPositionAndLimit(piFirst[pass], region.batchFirst[pass], region.batchLimit[pass]);
             Util.setPositionAndLimit(piCount[pass], region.batchFirst[pass], region.batchLimit[pass]);
 
@@ -344,6 +347,7 @@ public class NeoRenderer {
         }
 
         glBindVertexArray(0);
+        return rendered;
     }
 
     private void updateGLValues() {

src/main/java/makamys/neodymium/mixin/MixinRenderGlobal.java
@@ -30,10 +30,13 @@ abstract class MixinRenderGlobal {
         }
     }
     
-    @Inject(method = "renderSortedRenderers", at = @At(value = "HEAD"))
-    public void preRenderSortedRenderers(int startRenderer, int numRenderers, int renderPass, double partialTickTime, CallbackInfoReturnable cir) {
+    @Inject(method = "renderSortedRenderers",
+            at = @At(value = "HEAD"),
+            cancellable = true,
+            require = 1)
+    public void preRenderSortedRenderers(int startRenderer, int numRenderers, int renderPass, double partialTickTime, CallbackInfoReturnable<Integer> cir) {
         if(Neodymium.isActive()) {
-            Neodymium.renderer.preRenderSortedRenderers(renderPass, partialTickTime, sortedWorldRenderers);
+            cir.setReturnValue(Neodymium.renderer.preRenderSortedRenderers(renderPass, partialTickTime, sortedWorldRenderers));
         }
     }
     

src/main/java/makamys/neodymium/mixin/MixinWorldRenderer.java
@@ -10,11 +10,14 @@ import makamys.neodymium.renderer.NeoRenderer;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.EntityLivingBase;
+
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collections;
@@ -89,6 +92,25 @@ abstract class MixinWorldRenderer implements IWorldRenderer {
             nd$chunkMeshes.set(pass, cm);
             ChunkMesh.setCaptureTarget(cm);
         }
+    }
+
+    @Redirect(method = "preRenderBlocks",
+              at = @At(value = "INVOKE",
+                       target = "Lorg/lwjgl/opengl/GL11;glNewList(II)V"),
+              require = 1)
+    private void noNewList(int list, int mode) {
+        if (!Neodymium.isActive()) {
+            GL11.glNewList(list, mode);
+        }
+    }
+
+    @Redirect(method = "postRenderBlocks",
+              at = @At(value = "INVOKE",
+                       target = "Lorg/lwjgl/opengl/GL11;glEndList()V"),
+              require = 1)
+    private void noEndList() {
+        if (!Neodymium.isActive())
+            GL11.glEndList();
     }
     
     @Inject(method = "postRenderBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/Tessellator;draw()I"))

commit 2c0e73c7e3b510a4a51774958207e9c7094d6438

Reimplement vanilla world renderer stats in F3 overlay
Fixes regression in ad42f10, which caused all values to be 0

src/main/java/makamys/neodymium/renderer/NeoRenderer.java
@@ -14,6 +14,7 @@ import makamys.neodymium.renderer.attribs.AttributeSet;
 import makamys.neodymium.util.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumChatFormatting;
@@ -146,6 +147,10 @@ public class NeoRenderer {
                     sort(frameCount % 100 == 0, frameCount % Config.sortFrequency == 0);
 
                     initIndexBuffers();
+                    
+                    if(!Constants.KEEP_RENDER_LIST_LOGIC) {
+                        updateRenderGlobalStats();
+                    }
                 }
 
                 frameCount++;
@@ -206,7 +211,7 @@ public class NeoRenderer {
                         for (int j = piCount[i].position() - meshes; j < piCount[i].position(); j++) {
                             renderedQuads += piCount[i].get(j) / 4;
                         }
-                        if(Compat.isHodgepodgeSpeedupAnimationsEnabled() && !Constants.KEEP_RENDER_LIST_LOGIC) {
+                        if(Compat.isHodgepodgeSpeedupAnimationsEnabled() && !Constants.KEEP_RENDER_LIST_LOGIC) { 
                             // Hodgepodge hooks this method to decide what animations to play, make sure it runs
                             wr.getGLCallListForPass(i);
                         }
@@ -232,6 +237,26 @@ public class NeoRenderer {
             }
         }
         return false;
+    }
+    
+    private static void updateRenderGlobalStats() {
+        // Normally renderSortedRenderers does this, but we cancelled it
+        
+        RenderGlobal rg = Minecraft.getMinecraft().renderGlobal;
+        
+        for(WorldRenderer wr : rg.sortedWorldRenderers) {
+            ++rg.renderersLoaded;
+
+            if (wr.skipRenderPass[0]) {
+                ++rg.renderersSkippingRenderPass;
+            } else if (!wr.isInFrustum) {
+                ++rg.renderersBeingClipped;
+            } else if (rg.occlusionEnabled && !wr.isVisible) {
+                ++rg.renderersBeingOccluded;
+            } else {
+                ++rg.renderersBeingRendered;
+            }
+        }
     }
 
     private void mainLoop() {

src/main/resources/META-INF/neodymium_at.cfg
@@ -16,3 +16,11 @@ public net.minecraft.client.renderer.Tessellator field_78409_u #drawMode
 public net.minecraft.client.renderer.Tessellator field_78408_v #xOffset
 public net.minecraft.client.renderer.Tessellator field_78407_w #yOffset
 public net.minecraft.client.renderer.Tessellator field_78417_x #zOffset
+
+public net.minecraft.client.renderer.RenderGlobal field_72751_K # renderersLoaded
+public net.minecraft.client.renderer.RenderGlobal field_72744_L # renderersBeingClipped
+public net.minecraft.client.renderer.RenderGlobal field_72745_M # renderersBeingOccluded
+public net.minecraft.client.renderer.RenderGlobal field_72746_N # renderersBeingRendered
+public net.minecraft.client.renderer.RenderGlobal field_72747_O # renderersSkippingRenderPass
+public net.minecraft.client.renderer.RenderGlobal field_72774_t # occlusionEnabled
+public net.minecraft.client.renderer.RenderGlobal field_72768_k # sortedWorldRenderers